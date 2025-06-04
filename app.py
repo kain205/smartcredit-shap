0695b2944481a8676dad17e32a5b7a30bd503da0
hoang <hoangthilinhhuong1@gmail.com>
2025-06-04 15:05:20 +0700

    bg

--- app.py
+++ app.py
@@ -31,7 +31,20 @@ def train_model():
 clf, encoders, X_train = train_model()
 
 st.title('Dự đoán phê duyệt đơn vay & Giải thích bằng SHAP')
-
+st.markdown(
+        """
+        <style>
+        .stApp {
+            background-image: url("https://drive.google.com/uc?id=1GtV1i3q-3W3hkb0nP-Q-GoLAUaEcJOtB");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 # Input form
 def user_input_features():
     df = pd.read_csv('loan_data.csv')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+st.markdown(
+        """
+        <style>
+        .stApp {
+            background-image: url("https://drive.google.com/uc?id=1GtV1i3q-3W3hkb0nP-Q-GoLAUaEcJOtB");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
